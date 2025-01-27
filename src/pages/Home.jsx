226279c7f4f34566f37a1b5e73001630f247d167
gouttedooo <goudal.donovan@live.fr>
2025-01-27 22:38:37 +0100

    feat: ajout video accueil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home">
-      <div className="home__presentation"></div>
+      <div className="home__presentation">
+        <video
+          className="home__presentation__video"
+          autoPlay
+          muted
+          loop
+          poster="/assets/images/logo.jpg"
+        >
+          <source
+            src="/assets/videos/presentation-light.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 };
