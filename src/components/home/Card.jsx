48e4af19233e5f94836c589465612216791b1656
gouttedooo <goudal.donovan@live.fr>
2025-01-28 15:31:55 +0100

    style: ajout data des cards Home

--- src/components/home/Card.jsx
+++ src/components/home/Card.jsx
@@ -5,7 +5,26 @@ import React from "react";
  * @returns
  */
 const Card = () => {
-  return <div className="main__card">BOXES EXT</div>;
+  return (
+    <div className="main__card">
+      <div className="main__card__carousel">CAROUSEL</div>
+      <div className="main__card__content">
+        <h1>Boxes Ext</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptatibus, doloremque, voluptas, quia, quos, aspernatur, fugiat
+          doloribus, voluptatem, dolores, quisquam, quod, asperiores
+          consequuntur, voluptatum, doloremque, quibusdam, quos, aspernatur,
+          fugiat doloribus, voluptatem, dolores, quisquam, quod, asperiores
+          consequuntur, voluptatum, doloremque, quibusdam, quos, aspernatur,
+          fugiat doloribus, voluptatem, dolores, quisquam, quod, asperiores
+          consectetur, voluptatum, doloremque, quibusdam, quos, aspernatur,
+          fugiat doloribus, voluptatem, dolores, quisquam, quod, asperiores
+          quia, voluptatibus, doloremque
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/home/Presentation.jsx
+++ src/components/home/Presentation.jsx
@@ -5,12 +5,16 @@ const Presentation = () => {
     <div className="main__presentation">
       <h1>Les Ecuries de la Selle</h1>
       <p>
-        Depuis 2020, Les Écuries de la Selle accueillent les cavaliers
-        propriétaires dans un cadre chaleureux et convivial à Saulzoir. Dédiée
-        au bien-être des chevaux et à l'accompagnement des cavaliers, nos
+        Depuis 2021, Les Écuries de la Selle accueillent les cavaliers
+        propriétaires dans un cadre familial, chaleureux et convivial à
+        Saulzoir.
+        <br />
+        Dédiée au bien-être des chevaux et à l'accompagnement des cavaliers, nos
         écuries proposent des installations modernes et un service de qualité
-        pour répondre aux besoins de chaque propriétaire. Rejoignez-nous et
-        partagez votre passion dans une ambiance sereine et dynamique !
+        pour répondre aux besoins de chaque propriétaire.
+        <br />
+        Rejoignez-nous et partagez votre passion dans une ambiance sereine et
+        dynamique !
       </p>
     </div>
   );
