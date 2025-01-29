f00afb440e07b779b7bda5db3099a6aa2be1d117
gouttedooo <goudal.donovan@live.fr>
2025-01-29 23:50:46 +0100

    chore: Working on transition when clicking on last index. But doesnt work anymore. La transition se fait bien lorsqu'uon clique sur l'index 20 l'index 0*, mais elle génère une transition de 0 au quasi dernier index (logique) Il faut trouver un moyen de mettre une transition de 0 à l'index 0 -> index(max). Et ensuite la remettre à sa valeur classique, et faire de même de l'index(max -> index 0

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 
 const Carousel = ({ card }) => {
-  const [slideX, setSlideX] = useState(0);
+  const [slideX, setSlideX] = useState(-100);
+  const numberImages = card.carousel + 2;
   const handleSlide = (direction) => {
     const newX = slideX + direction;
-    const indexes = card.carousel;
-    if (newX > 0) {
-      setSlideX(-(indexes - 1) * 100);
+    const indexes = numberImages;
+    if (newX === 0) {
+      setSlideX(0);
+      //téléporte à la fin
+      setTimeout(() => setSlideX(-(indexes - 2) * 100), 300);
     } else if (newX < -(indexes - 1) * 100) {
       setSlideX(0);
     } else {
       setSlideX((prevState) => prevState + direction);
     }
+    console.log(newX);
   };
 
-  const isFilter = (index) => card.carousel - 1 === index && card.filter;
+  const isFilter = (index) => numberImages - 2 === index && card.filter;
 
   return (
     <div className="carousel--viewport">
@@ -23,35 +27,34 @@ const Carousel = ({ card }) => {
       <div
         className="carousel--slides"
         style={{
-          transform: `translateX(${slideX / card.carousel}%)`,
-          width: `calc(100% * ${card.carousel})`,
+          transform: `translateX(${slideX / numberImages}%)`,
+          width: `calc(100% * ${numberImages})`,
+          transition: `all ${
+            slideX == -(numberImages - 2) * 100 ? "0s " : "0.3s "
+          }ease-in-out`,
         }}
       >
-        {[...Array(card.carousel).keys()].map((_, index) => {
-          if (isFilter(index)) {
-            return (
-              <div
-                className={`carousel--item filter`}
-                style={{
-                  backgroundImage: `url(/assets/images/cards/${card.id}_filter.jpg)`,
-                }}
-                key={index}
-              >
-                <button>DÉCOUVRIR TOUTES NOS INSTALLATIONS</button>
-              </div>
-            );
-          } else {
-            return (
-              <div
-                className={`carousel--item `}
-                style={{
-                  backgroundImage: `url(/assets/images/cards/${card.id}_${index}.jpg)`,
-                }}
-                key={index}
-              ></div>
-            );
-          }
-        })}
+        {[...Array(numberImages).keys()].map((_, index) => (
+          <div
+            className={`carousel--item ${isFilter(index) ? "filter" : ""}`}
+            style={{
+              backgroundImage: `url(/assets/images/cards/${card.id}_${
+                isFilter(index)
+                  ? "filter"
+                  : index == 0
+                  ? card.carousel - 1
+                  : index == numberImages - 1
+                  ? 0
+                  : index - 1
+              }.jpg)`,
+            }}
+            key={index}
+          >
+            {isFilter(index) && (
+              <button>DÉCOUVRIR TOUTES NOS INSTALLATIONS</button>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
