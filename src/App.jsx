f38cca96580abca00ce0ade0444bea37c5e90206
gouttedooo <goudal.donovan@live.fr>
2025-01-31 22:29:52 +0100

    chore: Ajout page Proprietaires

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //pages
 import Home from "./pages/Home";
+import Proprietaires from "./pages/Proprietaires";
+import Contacts from "./pages/Contacts";
 import NotFound from "./pages/NotFound";
 //composants
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 //styles
 import "./styles/main.scss";
-import Contacts from "./pages/Contacts";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route path="/" element={<Home />} />
+          <Route path="proprietaires" element={<Proprietaires />} />
           <Route path="contacts" element={<Contacts />} />
           <Route path="*" element={<NotFound />} />
         </Route>
---
+++ src/components/IntroAnnexe.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+/**
+ * Affiche introduction des pages annexes
+ * Une image avec le titre de la page
+ * puis une légère description
+ * @returns
+ */
+const IntroAnnexe = () => {
+  return (
+    <div className="intro-annexe">
+      <div className="intro-annexe__bg-img"></div>
+      <div className="intro-annexe__description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+        sapiente voluptas obcaecati praesentium. Delectus, quas provident cumque
+        veritatis vel quam voluptas laudantium repellat quo eaque!
+      </div>
+    </div>
+  );
+};
+
+export default IntroAnnexe;
