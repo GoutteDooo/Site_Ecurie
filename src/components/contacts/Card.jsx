be3fa234b525f06a7845f81b8dd0a827ba5b6ebf
gouttedooo <goudal.donovan@live.fr>
2025-01-31 18:25:50 +0100

    chore(Contacts): Card structure setup

--- src/components/contacts/Card.jsx
+++ src/components/contacts/Card.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Card = ({ icon, content }) => {
-  return <div className="contacts__card"></div>;
+  return (
+    <div className="contacts__card">
+      <div className="contacts__card--icon">
+        <i className={`fa-solid fa-${icon}`}></i>
+      </div>
+      <div className="contacts__card--content">
+        <p>{content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
