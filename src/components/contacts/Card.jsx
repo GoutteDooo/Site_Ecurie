01d73e42b1fc882acd9093512442e70cdbdf085b
gouttedooo <goudal.donovan@live.fr>
2025-01-31 19:09:44 +0100

    chore(Contacts): Links Ok and searching for how to link number, email and address

--- src/components/contacts/Card.jsx
+++ src/components/contacts/Card.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Card = ({ icon, content }) => {
+const Card = ({ icon, content, link = false }) => {
   return (
-    <div className="contacts__card">
-      <div className="contacts__card--icon">
-        <i className={`fa-solid fa-${icon}`}></i>
-      </div>
-      <div className="contacts__card--content">
-        <p>{content}</p>
-      </div>
-    </div>
+    <>
+      {link ? (
+        <Link to={link}>
+          <div className="contacts__card">
+            <div className="contacts__card--icon">{icon}</div>
+            <div className="contacts__card--content">
+              <p className="contacts__card--link">{content}</p>
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <div className="contacts__card">
+          <div className="contacts__card--icon">{icon}</div>
+          <div className="contacts__card--content">
+            <p>{content}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
