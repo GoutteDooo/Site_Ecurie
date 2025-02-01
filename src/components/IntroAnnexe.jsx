5df0706ada61f56db335d2d756ca319dfb4b0c57
gouttedooo <goudal.donovan@live.fr>
2025-02-01 11:33:26 +0100

    feat(IntroAnnexes): Ajout boldage du texte descriptif

--- src/components/IntroAnnexe.jsx
+++ src/components/IntroAnnexe.jsx
@@ -7,26 +7,47 @@ import React from "react";
  * @returns
  */
 const IntroAnnexe = ({ annexe }) => {
+  const parseDescription = () => {
+    const bold = annexe.bold;
+    const desc = annexe.description;
+    if (bold) {
+      return desc.replace(
+        new RegExp(bold.join("|"), "g"),
+        (match) => `<strong>${match}</strong>`
+      );
+    } else {
+      return desc;
+    }
+  };
+  const parsedDescription = parseDescription();
   return (
     <div className="intro-annexe">
-      <div className="intro-annexe__bg-video">
+      <div className="intro-annexe__bg-media">
         <h1>{annexe.title}</h1>
-        <video
-          className="intro-annexe__bg-video--video"
-          autoPlay
-          muted
-          loop
-          poster="/assets/images/accueil_mobile.jpg"
-        >
-          <source
-            src={`/assets/videos/intro-${annexe.videoLink}-light.mp4`}
-            type="video/mp4"
+        {annexe.videoLink && (
+          <video
+            className="intro-annexe__bg-video--video"
+            autoPlay
+            muted
+            loop
+            poster="/assets/images/accueil_mobile.jpg"
+          >
+            <source
+              src={`/assets/videos/intro-${annexe.videoLink}-light.mp4`}
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        )}
+        {annexe.imageLink && (
+          <img
+            className="intro-annexe__bg-image"
+            src={`/assets/images/annexes/${annexe.imageLink}.jpg`}
           />
-          Your browser does not support the video tag.
-        </video>
+        )}
       </div>
       <div className="intro-annexe__description">
-        <p>{annexe.description}</p>
+        <p dangerouslySetInnerHTML={{ __html: parsedDescription }}></p>
       </div>
     </div>
   );
