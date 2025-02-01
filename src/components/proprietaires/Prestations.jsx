1e998c5120bad74f89fe64040100292544954c61
gouttedooo <goudal.donovan@live.fr>
2025-02-01 22:17:13 +0100

    chore(variables): Ajout des variables en JS

--- src/components/proprietaires/Prestations.jsx
+++ src/components/proprietaires/Prestations.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Bubble from "../Bubble";
 
 const Prestations = () => {
   return (
     <div className="proprietaires__prestations">
       <h1>Nos Prestations</h1>
+      <Bubble bgColor="#5c6bc0" textColor="white" opacity="0.8"></Bubble>
     </div>
   );
 };
---
+++ src/utils/variables.js
@@ -0,0 +1,15 @@
+const MAIN_COLOR = "#5c6bc0";
+const SECONDARY_COLOR = "#3f51b5";
+const THIRD_COLOR = "#42a5f5";
+const TEXT_COLOR = "black";
+const BG_COLOR = "#eeeeee";
+const BG_COLOR_DARK = "#f5f5f5";
+
+export {
+  MAIN_COLOR,
+  SECONDARY_COLOR,
+  THIRD_COLOR,
+  TEXT_COLOR,
+  BG_COLOR,
+  BG_COLOR_DARK,
+};
