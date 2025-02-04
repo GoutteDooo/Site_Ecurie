e8c0ed6ec6b852235e4d68f4754147157fcf1b02
gouttedooo <goudal.donovan@live.fr>
2025-02-04 16:25:45 +0100

    feat(Cours): Add Infos supplementaires

--- src/components/cours/Tarifs.jsx
+++ src/components/cours/Tarifs.jsx
@@ -1,34 +1,40 @@
 import React from "react";
 import Bubble from "../../components/Bubble";
-import { BG_COLOR } from "../../utils/variables";
+import { BG_COLOR, SECONDARY_COLOR } from "../../utils/variables";
+import { CONTENT_PRESTATIONS, CONTENT_INFOS } from "../../data/Tarifs_data";
 
 /**
  * Display les tarifs des cours
  * @returns
  */
 const Tarifs = () => {
-  const content = [
-    <>
-      <p>Cours Individuels</p>
-      <em>30€/heure & 250€ pour 10 heures</em>
-    </>,
-    <>
-      <p>Cours Collectifs</p>
-      <em>15€/heure & 120€ pour 10 heures</em>
-    </>,
-  ];
-  const MAIN_COLOR__OPACITY = "#5c6bc099";
+  const MAIN_COLOR_OPACITY = "#5c6bc0cc";
+  const SECONDARY_COLOR_OPACITY = "#3f51b5cc";
+
   return (
     <div className="cours__tarifs">
       <div className="cours__tarifs--bg"></div>
-      <h1>Les Tarifs des Cours</h1>
-      {content.map((c, i) => (
+      <h1>Nos Prestations</h1>
+      {CONTENT_PRESTATIONS.map((c, i) => (
         <Bubble
-          bgColor={MAIN_COLOR__OPACITY}
+          bgColor={SECONDARY_COLOR_OPACITY}
           textColor={BG_COLOR}
           content={c}
           key={i}
-          borderColor="none"
+          borderColor={SECONDARY_COLOR_OPACITY}
+        />
+      ))}
+      <h1 style={{ fontSize: "1.4rem", backgroundColor: MAIN_COLOR_OPACITY }}>
+        Informations supplémentaires
+      </h1>
+      {CONTENT_INFOS.map((c, i) => (
+        <Bubble
+          bgColor={MAIN_COLOR_OPACITY}
+          textColor={BG_COLOR}
+          content={c}
+          key={i}
+          size={70}
+          borderColor={MAIN_COLOR_OPACITY}
         />
       ))}
     </div>
---
+++ src/data/Tarifs_data.jsx
@@ -0,0 +1,25 @@
+export const CONTENT_PRESTATIONS = [
+  <>
+    <p style={{ fontWeight: "bold", fontSize: "1.1rem" }}>Cours Individuels</p>
+    <em>30€/heure & 250€ pour 10 heures</em>
+  </>,
+  <>
+    <p style={{ fontWeight: "bold", fontSize: "1.1rem" }}>Cours Collectifs</p>
+    <em>15€/heure & 120€ pour 10 heures</em>
+  </>,
+  <>
+    <p style={{ fontSize: "1.1rem" }}>Monte/Longe/Sorties des chevaux</p>
+  </>,
+  <>
+    <p style={{ fontSize: "1.1rem" }}>Cours Classiques et Éthologiques</p>
+  </>,
+];
+
+export const CONTENT_INFOS = [
+  <p>Monitrice Diplômée BPJEPS & BFEEE1</p>,
+  <p>Cours Classiques et Éthologiques</p>,
+  <p>Accompagnement dans les concours</p>,
+  <p>Passage de galops FFE</p>,
+  <p>Coachings concours club amateur, dressage & sauts</p>,
+  <p>Prise de Licence, tarifs suivant FFE</p>,
+];
