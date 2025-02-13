28300d39e659d7c38f47b06037a5e56215edae7e
gouttedooo <goudal.donovan@live.fr>
2025-02-13 11:45:32 +0100

    style(Cours/prestations): responsive ok

--- src/components/cours/Tarifs.jsx
+++ src/components/cours/Tarifs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Bubble from "../../components/Bubble";
 import { BG_COLOR } from "../../utils/variables";
 import { CONTENT_PRESTATIONS } from "../../data/Tarifs_data";
+import ScrollArrow from "../ScrollArrow";
 
 /**
  * Display les tarifs des cours
@@ -9,20 +10,26 @@ import { CONTENT_PRESTATIONS } from "../../data/Tarifs_data";
  */
 const Tarifs = () => {
   const SECONDARY_COLOR_OPACITY = "#3f51b5cc";
+  console.log(window.innerWidth);
 
   return (
     <div className="cours__tarifs">
       <div className="cours__tarifs--bg"></div>
       <h1>Nos Prestations</h1>
-      {CONTENT_PRESTATIONS.map((c, i) => (
-        <Bubble
-          bgColor={SECONDARY_COLOR_OPACITY}
-          textColor={BG_COLOR}
-          content={c}
-          key={i}
-          borderColor={SECONDARY_COLOR_OPACITY}
-        />
-      ))}
+      <div className="cours__tarifs--content">
+        {CONTENT_PRESTATIONS.map((c, i) => (
+          <Bubble
+            bgColor={SECONDARY_COLOR_OPACITY}
+            textColor={BG_COLOR}
+            content={c}
+            key={i}
+            borderColor={SECONDARY_COLOR_OPACITY}
+          />
+        ))}
+        {window.innerWidth < 1280 ? (
+          <ScrollArrow size={70} scrolling={2} />
+        ) : null}
+      </div>
     </div>
   );
 };
